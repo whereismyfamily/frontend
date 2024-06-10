932c05fa9d6a028cdf6001c71aefabd5a541aabe
LP <lp@tsl.io>
2024-06-10 19:29:15 -0300

    kkk

--- js/chat.js
+++ js/chat.js
@@ -1,19 +1,29 @@
 const chat = document.getElementById('chat')
 
-const truncate = (string, length) => {
-    console.log(string.length, length)
-    return {truncatedString: string.substring(0, length), isTruncated: string.length > length}
-};
+window.onload = () => {
+    const loading = document.createElement('div')
+    loading.className = 'd-flex justify-content-center align-items-center'
+    loading.innerHTML = `
+        <svg id="svgLoading" width="100" height="100" viewBox="0 0 250 250" class="circular-progress">
+            <circle class="bg"></circle>
+            <circle class="fg"></circle>
+        </svg>
+    `
+    chat.appendChild(loading)
+}
 
-const populateChats = async (users) => {
+const truncate = (string, length) => ({truncatedString: string.substring(0, length), isTruncated: string.length > length})
+
+const populateChats = async ({users, messages}) => {
+    const loading = document.getElementById("svgLoading")
+    loading.remove()
     for (userIndex in users) {
         const hour = Math.floor(Math.random() * 24)
         const minute = Math.floor(Math.random() * 60)
         const user = users[userIndex]
+        const message = messages[userIndex]
         const messageElement = document.createElement('div')
         const hr = document.createElement('hr')
-        const messageResponse = await fetch('https://api-random.vercel.app/').then(res => res.json())
-        const message = truncate(messageResponse.mensage, 100)
 
         messageElement.className = 'd-flex align-items-center'
         messageElement.innerHTML = `
@@ -29,4 +39,12 @@ const populateChats = async (users) => {
     }
 }
 
-fetch('https://randomuser.me/api/?results=9&nat=br').then(res => res.json()).then(res => populateChats(res.results))
+fetch('https://randomuser.me/api/?results=9&nat=br').then(res => res.json()).then(async res => {
+    const messagePromises = []
+    for (let i=0; i<9; i++) {
+        const messagePromise = fetch('https://api-random.vercel.app/').then(res => res.json()).then(res => truncate(res.mensage, 100))
+        messagePromises.push(messagePromise)
+    }
+    const messages = await Promise.all(messagePromises)
+    return {messages, users: res.results}
+}).then(res => populateChats(res))
